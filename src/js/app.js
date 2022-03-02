0ecf9700de7b122d5b7ec44203cf6e7adf174420
Carlos <lamark.barros@gmail.com>
2022-03-01 23:32:48 -0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    var taskbar = document.querySelector(".taskbar");
+    header.classList.toggle("sticky", window.scrollY>90)
+})
+
