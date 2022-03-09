fd7d2145f04473b3997319ba7d61eb96f623fc82
Carlos <lamark.barros@gmail.com>
2022-03-08 21:37:17 -0300

    implementa skills mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     var taskbar = document.querySelector(".taskbar");
-    header.classList.toggle("sticky", window.scrollY>0)
+    header.classList.toggle("sticky", window.scrollY>100)
 })
 
